82998741f1ee78753e6e989a4721ea0bb332887d
lars <larstomaslian@gmail.com>
2021-10-02 17:09:46 +0200

    Add unit test with src project dependency

---
+++ test/ESWebApiTests/WeatherForecastTests.cs
@@ -0,0 +1,24 @@
+﻿using ESWebApi;
+using Xunit;
+
+namespace ESWebApiTests
+{
+    public class WeatherForecastTests
+    {
+        [Fact]
+        public void CanConvertCelsiusToFahrenheit()
+        {
+            // Arrange
+            var weatherForecast = new WeatherForecast
+            {
+                TemperatureC = 20
+            };
+
+            // Act
+            var temperatureF = weatherForecast.TemperatureF;
+
+            // Assert
+            Assert.Equal(67, temperatureF);
+        }
+    }
+}
